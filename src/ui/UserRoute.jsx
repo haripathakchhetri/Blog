2256e8a80be8df7d2425a9a26863b4d35e78fb63
haripathakchhetri <pathakhari570@gmail.com>
2024-09-06 17:38:51 +0545

    ch

--- src/ui/UserRoute.jsx
+++ src/ui/UserRoute.jsx
@@ -6,8 +6,6 @@ const UserRoute = () => {
 
   const user = useAuth();
 
-
-
-  return user ? <Outlet /> : <Navigate to={'/'} replace /> //user xa vane jaha jana man xa teha jani haina vane / route ma jane..replace le chai jun page bata aaxa tyo page ma back jana mildaina.
+  return user ? <Outlet /> : <Navigate to={'/login'} replace /> //user xa vane jaha jana man xa teha jani haina vane / route ma jane..replace le chai jun page bata aaxa tyo page ma back jana mildaina.
 }
 export default UserRoute
